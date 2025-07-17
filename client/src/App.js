d71dc190a689742589ff3db3f868f25f5c2605cb
jerw04 <jerwinmanuel04@gmail.com>
2025-07-17 13:20:38 +0530

    Initial project setup: backend + frontend + Tailwind config

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="text-3xl font-bold text-blue-600 text-center p-10">
+      Freelance Marketplace 🚀 (Tailwind is working!)
+    </div>
+  );
+}
+
+export default App;
